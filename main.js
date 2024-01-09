c5f0a4aee2515646daab17c2dde6ef6187a998d8
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2024-01-09 12:46:40 -0300

    ADD: Custom-Button

---
+++ main.js
@@ -0,0 +1,14 @@
+app.set('view engine', 'pug')
+
+const express = require('express');
+const app = express();
+
+
+
+app.set('view engine', 'pug');
+
+$(document).ready(function(){
+    $('header button').click(function(){
+        alert('AGORA VOCE VAI PEGAR SEU MERDA')
+    })
+})
